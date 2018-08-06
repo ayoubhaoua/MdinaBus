14b526335a9b4a9755504328753c7705e5eb1297
HAOUA Ayoub <dr.ayoubhaoua@gmail.com>
2018-08-06 14:10:37 +0000

    js html repo point ramassage

---
+++ src/main/java/com/MdinaBus/Repositories/Pt_ramassageRepo.java
@@ -0,0 +1,10 @@
+package com.MdinaBus.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import com.MdinaBus.Models.Pt_ramassage;
+
+public interface Pt_ramassageRepo extends CrudRepository<Pt_ramassage, Long>{
+
+}
