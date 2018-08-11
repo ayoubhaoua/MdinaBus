7860ae022a52a6b67ad64d4f96d8cdb7345fa65b
unknown <naimasahih76@gmail.com>
2018-08-11 13:52:34 +0000

    fragment

--- src/main/java/com/MdinaBus/Repositories/CircuitsRepo.java
+++ src/main/java/com/MdinaBus/Repositories/CircuitsRepo.java
@@ -1,9 +1,12 @@
 package com.MdinaBus.Repositories;
 
+import java.util.Date;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.MdinaBus.Models.Circuit;
 
 public interface CircuitsRepo extends CrudRepository<Circuit, Long> {
+	
 
 }
