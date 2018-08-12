f18f0810f35f264d0ebeb0a1597cc87c252efd27
HAOUA Ayoub <dr.ayoubhaoua@gmail.com>
2018-08-12 14:17:39 +0000

    repush

--- src/main/java/com/MdinaBus/Repositories/CircuitsRepo.java
+++ src/main/java/com/MdinaBus/Repositories/CircuitsRepo.java
@@ -1,13 +1,9 @@
 package com.MdinaBus.Repositories;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-=======
-import java.util.Date;
-
->>>>>>> 7860ae022a52a6b67ad64d4f96d8cdb7345fa65b
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,11 +13,8 @@ import com.MdinaBus.Models.Circuit;
 
 @Repository
 public interface CircuitsRepo extends CrudRepository<Circuit, Long> {
-<<<<<<< HEAD
+
 	@Query("select c from  Circuit c where c.id_service=?1")
 	List<Circuit> findById_service(@Param("id_service") long id_service);
-=======
-	
->>>>>>> 7860ae022a52a6b67ad64d4f96d8cdb7345fa65b
 
 }
